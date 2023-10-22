598032258c375fc2310b394d830076f8b0f05cca
HighDrizzDev <usvirux@gmail.com>
2023-10-21 20:46:02 -0400

    Agregue funcionabilidad a FacturacionRazor. Se cargan los turnos pendientes con sus pruebasDeLaboratorio en FacturacionRazor. Modifique el diseno de interfaces, MainLayout y NavMenu

--- Proyecto2Maui/Modelos/PruebaLab.cs
+++ Proyecto2Maui/Modelos/PruebaLab.cs
@@ -8,7 +8,7 @@ namespace Proyecto2Maui.Modelos
 {
     public class PruebaLab
     {
-        public int idPrueba { get; set; }
+        public int idPruebaLab { get; set; }
         public string nombrePrueba { get; set; }
         public decimal? precio { get; set; }
         public bool isChecked { get; set; } = false;
