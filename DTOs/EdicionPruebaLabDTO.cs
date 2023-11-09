d8e9114e8d4b8c4eeb0565e42f27ef2bccd2305a
Lean <Lean@DESKTOP-DAICI6I>
2023-11-09 04:27:10 +0100

    Implementacion mantenedorPruebaLab

--- DTOs/EdicionPruebaLabDTO.cs
+++ DTOs/EdicionPruebaLabDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,12 @@ namespace DTOs
 {
     public class EdicionPruebaLabDTO
     {
-        public int IdPruebaLab { get; set; }
-        public string NombrePrueba { get; set; }
-        public string Descripcion { get; set; }
-        public decimal Precio { get; set; }
+        [Required(ErrorMessage = "El Id de la prueba de laboratorio es requerido")]
+        public int idPruebaLab { get; set; }
+        [Required(ErrorMessage = "El Nombre de la prueba de laboratorio es requerido")]
+        public string nombrePrueba { get; set; }
+        [Required(ErrorMessage = "La descripcion de la prueba de laboratorio es requerido")]
+        public string descripcion { get; set; }
+        public decimal? precio { get; set; }
     }
 }
--- Proyecto2Maui/Modelos/PruebaLab.cs
+++ Proyecto2Maui/Modelos/PruebaLab.cs
@@ -10,6 +10,7 @@ namespace Proyecto2Maui.Modelos
     {
         public int idPruebaLab { get; set; }
         public string nombrePrueba { get; set; }
+        public string descripcion { get; set; }
         public decimal? precio { get; set; }
         public bool isChecked { get; set; } = false;
     }
